420eef41c5ee4902e8cc436039f847f10418df58
WeiZiboSheep <wzblyl@163.com>
2018-10-30 16:03:08 +0800

    Update WebApplication1/WebApplication1/Controllers/DefaultController.cs

--- WebApplication1/WebApplication1/Controllers/DefaultController.cs
+++ WebApplication1/WebApplication1/Controllers/DefaultController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     {
         public IActionResult Index()
         {
-            var result = "11111";
+            var result = "22222";
             return View();
         }
     }
